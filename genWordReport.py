80a20b9427b8041a78c26bc62174f3cfb22cb1be
jason3e7 <jason3e7@gmail.com>
2015-09-15 17:53:13 +0800

    from excle to word

--- genWordReport.py
+++ genWordReport.py
@@ -1,9 +1,47 @@
+# -*- coding: utf8 -*-
+#coding=utf-8 
+
+import sys
+reload(sys)
+print sys.getdefaultencoding()
+sys.setdefaultencoding('utf-8')
+print sys.getdefaultencoding()
+
 import win32com.client
-filepath = "../RGtool/report/hello.docx"
+
+
+wordFilePath = "../RGtool/report/hello.docx"
+excelFilePath = "../RGtool/resource/test.xlsx"
+
+excelapp = win32com.client.Dispatch("Excel.Application")
+excelapp.Visible = 0
+excelxls = excelapp.Workbooks.Open(excelFilePath)
+
+ws = excelxls.Worksheets("Sheet1")
+used = ws.UsedRange
+nrows = used.Row + used.Rows.Count
+ncols = used.Column + used.Columns.Count
+
+#print nrows
+#print ncols
+'''
+for i in range(1, nrows):
+	for j in range(1, ncols):
+		#print i
+		#print j
+		print ws.Cells(i, j)
+'''
+#print ws.Cells(2, 2)
+
+
+#data = excelapp.Range("A1")
+#print data.value
+
 
 wordapp = win32com.client.Dispatch("Word.Application") # Create new Word Object
 wordapp.Visible = 0 # Word Application should`t be visible
 worddoc = wordapp.Documents.Add() # Create new Document Object
+'''
 worddoc.PageSetup.Orientation = 1 # Make some Setup to the Document:
 worddoc.PageSetup.LeftMargin = 20
 worddoc.PageSetup.TopMargin = 20
@@ -13,5 +51,17 @@ worddoc.Content.Font.Size = 11
 worddoc.Content.Paragraphs.TabStops.Add (100)
 worddoc.Content.Text = "Hello world!"
 worddoc.Content.MoveEnd
-worddoc.SaveAs(filepath)
+'''
+rng = worddoc.Range(0,0)
+for i in range(2, nrows):
+	for j in range(2, ncols):
+		rng.InsertAfter(ws.Cells(1, j))
+		rng.InsertAfter(" : ")
+		rng.InsertAfter(unicode(ws.Cells(i, j)))
+		rng.InsertAfter("\r\n")
+	rng.InsertAfter("\r\n\r\n")
+
+worddoc.SaveAs(wordFilePath)
 wordapp.Quit() # Close the Word Application
+
+
